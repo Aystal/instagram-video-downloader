2e6cee590421058f778ccb3bb094084816617768
Aystal <nachrewe@gmail.com>
2025-05-21 18:37:04 -0300

    Update app.py

--- app.py
+++ app.py
@@ -64,4 +64,5 @@ def download_file(filename):
     return send_from_directory("downloads", filename, as_attachment=True)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
